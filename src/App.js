77fac9ca9342535726bb8edbea7aa77ca5cc91b9
Abelard Bautista <abelardbautista@gmail.com>
2020-07-01 23:26:10 -0700

    Created page skeleton.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
 // import logo from "./logo.svg";
 // import "./App.css";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className="text-center">City Name</Col>
+        </Row>
+        <Row>
+          <Col>Temperature</Col>
+        </Row>
+        <Row>
+          <Col>Weather</Col>
+          <Col>Icon</Col>
+        </Row>
+        <Row>
+          <Col>Humidity</Col>
+          <Col>Wind</Col>
+          <Col>UV</Col>
+        </Row>
+        <Row>
+          <Col>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Temperature</th>
+                  <th>Condition</th>
+                  <th>Humidity</th>
+                  <th>Wind</th>
+                  <th>UV Index</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+        <Row>
+          <Col>Search</Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
