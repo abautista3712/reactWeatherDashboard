19f68b7c13fbc3a96fc0908f04fe37f4e0321c3c
Abelard Bautista <abelardbautista@gmail.com>
2020-07-06 23:44:12 -0700

    Background image styling moved to inline on App.js level.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import CurrentWeather from "./Components/CurrentWeather";
 import FiveDayTable from "./Components/FiveDayTable";
 import SearchCity from "./Components/SearchCity";
 // import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
+
+const clouds = "./assets/images/clouds.jpg";
 
 function App() {
   return (
-    <Container className="text-center backgroundImg">
+    <Container
+      className="text-center"
+      style={{ backgroundImage: `url(${clouds})` }}
+    >
       <CurrentWeather />
       <FiveDayTable />
       <SearchCity />
