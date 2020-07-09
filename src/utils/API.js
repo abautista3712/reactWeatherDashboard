3ea8a48c20e393272f4c4dadf243a5e5b1fd1abe
Abelard Bautista <abelardbautista@gmail.com>
2020-07-09 00:01:10 -0700

    Changed API key.

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   getWeather: function () {
     return axios.get(
-      "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=2d2e3d50a761f51d222ae328e374ca3b"
+      `https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=${process.env.REACT_APP_API_KEY}`
     );
   },
 };
