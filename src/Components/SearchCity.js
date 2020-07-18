d1e759cb63555c0e51a2e2a982d34ab64d4b2bac
Abelard Bautista <abelardbautista@gmail.com>
2020-07-18 15:28:17 -0700

    Added search prototype.

--- src/Components/SearchCity.js
+++ src/Components/SearchCity.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+const people = [
+  "Siri",
+  "Alexa",
+  "Google",
+  "Facebook",
+  "Twitter",
+  "LinkedIn",
+  "SinkedIn",
+];
+
 function SearchCity() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    const results = people.filter((person) =>
+      person.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
   return (
-    <Row>
-      <Col style={{ borderStyle: "solid" }}>Search Input</Col>
-      <Col style={{ borderStyle: "solid" }} xs={2} sm={2} md={2} lg={2} xl={2}>
-        Search
-      </Col>
-    </Row>
+    <form>
+      <Row>
+        <Col style={{ borderStyle: "solid" }}>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+          <ul>
+            {searchResults.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </Col>
+        <Col
+          style={{ borderStyle: "solid" }}
+          xs={2}
+          sm={2}
+          md={2}
+          lg={2}
+          xl={2}
+        >
+          Search
+        </Col>
+      </Row>
+    </form>
   );
 }
 
