4bd4e2d3b100862fa3f8b855ac80efa6f283d795
Abelard Bautista <abelardbautista@gmail.com>
2020-07-18 17:42:18 -0700

    Fixed time format.

--- src/Components/CurrentDate.js
+++ src/Components/CurrentDate.js
@@ -4,10 +4,10 @@ import moment from "moment";
 
 function CurrentDate() {
   //   const [date] = useState(moment().format("LLL"));
-  const [date, setDate] = useState(moment().format("LLL"));
+  const [date, setDate] = useState(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
   setTimeout(function () {
-    setDate(moment().format("LLL"));
+    setDate(moment().format("MMMM Do YYYY, h:mm:ss a"));
   }, 1000);
 
   return <Col>{date};</Col>;
