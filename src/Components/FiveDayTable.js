a46b313bda8d3383de9306a7da394c763377e19e
Abelard Bautista <abelardbautista@gmail.com>
2020-07-24 03:18:09 -0700

    Mapped temp into first row.

--- src/Components/FiveDayTable.js
+++ src/Components/FiveDayTable.js
@@ -39,24 +39,20 @@ function FiveDayTable() {
       setIsLoading(true);
       try {
         const fetcher = await API.getFiveDayForecast("Los Angeles");
-        const response = await fetcher.data.list;
+        const response = await fetcher.data.list.filter(
+          (rawData, index) =>
+            (index === 2) |
+            (index === 10) |
+            (index === 18) |
+            (index === 26) |
+            (index === 34)
+        );
         setWeather(response);
       } catch (err) {
         console.log("Error");
       } finally {
         setIsLoading(false);
       }
-      //  .then((res) => {
-      //     setWeather(
-      //       res.data.list
-      // .filter(
-      //   (index) =>
-      //     (index === 2) |
-      //     (index === 10) |
-      //     (index === 18) |
-      //     (index === 26) |
-      //     (index === 34)
-      // )
       // .map((filteredData) => {
       //   return filteredData;
       //   // console.log(filteredData);
@@ -72,82 +68,94 @@ function FiveDayTable() {
 
   return (
     <Row>
-      {isLoading ? console.log("Loading") : console.log(weather[0].main)}
-      <Col>
-        <div
-          className="mb-1 fontStyle"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
-        >
-          5-Day Forecast
-        </div>
-        <Table borderless striped size="sm">
-          <thead
-            className="fontStyle"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+      {isLoading ? (
+        console.log("Loading")
+      ) : (
+        <Col>
+          <div
+            className="mb-1 fontStyle"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
           >
-            <tr>
-              <th>Date</th>
-              <th>Temperature</th>
-              <th>Condition</th>
-              <th>Humidity</th>
-              <th>Wind</th>
-              {/* <th>UV Index</th> */}
-            </tr>
-          </thead>
-          <tbody
-            className="fontStyle"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
-          >
-            {/* Tomorrow */}
-            <tr>
-              <td>{moment().add(1, "days").format("l")}</td>
-              <td>{temperature}°</td>
-              <td>{condition}</td>
-              <td>{humidity} %</td>
-              <td>
-                {wind} m/s {windDirection}
-              </td>
-              {/* <td>{UV}</td> */}
-            </tr>
-            {/* +2 Days */}
-            <tr>
-              <td>{moment().add(2, "days").format("l")}</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              {/* <td>UV</td> */}
-            </tr>
-            {/* +3 Days */}
-            <tr>
-              <td>{moment().add(3, "days").format("l")}</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              {/* <td>UV</td> */}
-            </tr>
-            {/* +4 Days */}
-            <tr>
-              <td>{moment().add(4, "days").format("l")}</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              {/* <td>UV</td> */}
-            </tr>
-            {/* +5 Days */}
-            <tr>
-              <td>{moment().add(5, "days").format("l")}</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              {/* <td>UV</td> */}
-            </tr>
-          </tbody>
-        </Table>
-      </Col>
+            5-Day Forecast
+          </div>
+          <Table borderless striped size="sm">
+            <thead
+              className="fontStyle"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+            >
+              <tr>
+                <th>Date</th>
+                <th>Temperature</th>
+                <th>Condition</th>
+                <th>Humidity</th>
+                <th>Wind</th>
+                {/* <th>UV Index</th> */}
+              </tr>
+            </thead>
+            <tbody
+              className="fontStyle"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
+            >
+              {/* Tomorrow */}
+              <tr>
+                <td>{moment().add(1, "days").format("l")}</td>
+                {/* <td> */}
+                {weather.map((filteredData) => {
+                  return (
+                    console.log(filteredData),
+                    (<td>{filteredData.main.temp}°</td>)
+                  );
+                  // <td>{filteredData.main</td>;
+                })}
+
+                {/* </td> */}
+                <td>{condition}</td>
+                <td>{humidity} %</td>
+                <td>
+                  {wind} m/s {windDirection}
+                </td>
+                {/* <td>{UV}</td> */}
+              </tr>
+              {/* +2 Days */}
+              <tr>
+                <td>{moment().add(2, "days").format("l")}</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                {/* <td>UV</td> */}
+              </tr>
+              {/* +3 Days */}
+              <tr>
+                <td>{moment().add(3, "days").format("l")}</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                {/* <td>UV</td> */}
+              </tr>
+              {/* +4 Days */}
+              <tr>
+                <td>{moment().add(4, "days").format("l")}</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                {/* <td>UV</td> */}
+              </tr>
+              {/* +5 Days */}
+              <tr>
+                <td>{moment().add(5, "days").format("l")}</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                {/* <td>UV</td> */}
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      )}
     </Row>
   );
 }
