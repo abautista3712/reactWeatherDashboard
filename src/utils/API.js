8fbcf982b25a74ce727d4e2c678a07944dfb1e8f
Abelard Bautista <abelardbautista@gmail.com>
2020-07-25 00:33:35 -0700

    Added getCity. Converted getWeather to Imperial units.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export default {
+  getCity: function (query) {
+    return axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${process.env.REACT_APP_API_KEY}`
+    );
+  },
   getWeather: function (query) {
     return axios
       .get(
@@ -10,7 +15,7 @@ export default {
         const lat = res.data.city.coord.lat;
         const lon = res.data.city.coord.lon;
         return axios.get(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${process.env.REACT_APP_API_KEY}`
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${process.env.REACT_APP_API_KEY}`
         );
       });
   },
